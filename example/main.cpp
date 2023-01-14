04567acfcfed8aa16b4fd08cb6f2563ad2e2f3b8
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2023-01-14 11:37:18 +0300

    Add macros.h

--- example/main.cpp
+++ example/main.cpp
@@ -1,17 +1,29 @@
 #include <cpptoolkit/utest/utest.h>
+#include <cpptoolkit/utest/macros.h>
 #include <cassert>
 
 void test_f() {
-  TK_ASSERT_EQUAL(5, 6);
+  TK_TEST_EQUAL(5, 6);
 }
 
 void test_t() {
-  TK_ASSERT_TRUE(5 == 6);
+  TK_TEST_TRUE(5 == 6);
+}
+
+void test_d() {
+  TK_TEST_FALSE(true)
+}
+
+void test_null() {
+  int d = 5;
+  TK_TEST_IS_NULL(&d);
 }
 
 int main() {
-  assert(true);
-  test_f();
-  test_t();
-  std::cout << GREEN_COLOR_START << "Hello!" << GREEN_COLOR_STOP << std::endl;
+  // test_f();
+  // test_t();
+  // test_d();
+  test_null();
+  std::cout << GREEN_COLOR_START << "Hello!" << COLOR_STOP << std::endl;
 }
+
---
+++ include/cpptoolkit/utest/macros.h
@@ -0,0 +1,39 @@
+#ifndef CPPTOOLKIT_UTEST_MACROS_H_
+#define CPPTOOLKIT_UTEST_MACROS_H_
+
+#define RED_COLOR_START "\033[1;31m"
+#define GREEN_COLOR_START "\033[1;32m"
+#define COLOR_STOP "\033[0m\n"
+
+#define FAIL_TEST_OUT(x) \
+  std::cerr << RED_COLOR_START  \
+      << "Test:\t" << __func__ << " failed" << std::endl          \
+      << "Error:\t" << x << std::endl \
+      << "Where:\t" << __FILE__ << ":" << __LINE__ << COLOR_STOP;\
+
+#define TK_TEST_EQUAL(expected, actual) \
+  if (expected != actual) {             \
+    FAIL_TEST_OUT(expected << " is not equal to " << actual)\
+    return;\
+  } 
+
+#define TK_TEST_TRUE(val)       \
+  if (!static_cast<bool>(val)) {\
+    FAIL_TEST_OUT(#val << " is not true")\
+    return;\
+  }
+
+#define TK_TEST_FALSE(val)      \
+  if (static_cast<bool>(val)) { \
+    FAIL_TEST_OUT(#val << " is not false")\
+    return;\
+  }
+
+#define TK_TEST_IS_NULL(ptr)\
+  if (ptr != nullptr) {     \
+    FAIL_TEST_OUT("pointer is not null")\
+    return;\
+  }
+
+#endif // CPPTOOLKIT_UTEST_MACROS_H_
+
