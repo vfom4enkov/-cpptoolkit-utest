c062acc4f2611aeacf570f3e48d00a2821134357
Vladimir Fomchenkov <v.fom4enkov@gmail.com>
2023-01-17 13:38:42 +0300

    Add header.h, remove utest(.h/.cpp)

--- include/cpptoolkit/test/utest.h
+++
@@ -1,7 +0,0 @@
-#ifndef CPPTOOLKIT_TEST_UTEST_H_
-#define CPPTOOLKIT_TEST_UTEST_H_
-
-#include <iostream>
-
-#endif // CPPTOOLKIT_TEST_UTEST_H_
-
--- src/utest.cpp
+++
@@ -1,2 +0,0 @@
-#include "cpptoolkit/test/utest.h"
-
